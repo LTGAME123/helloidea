5c50b507dfb75acaea376daea4b61361f013a9b8
LTGAME123 <651456651@qq.com>
2019-11-04 10:07:53 +0800

    Initial commit

---
+++ src/Demo.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("hello idea");
+        Date date = new Date();
+    }
+
+
+
+}
---
+++ src/Users.java
@@ -0,0 +1,8 @@
+public class Users {
+    public static void main(String[] args) {
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("循环" + i +"次");
+
+        }
+    }
+}
